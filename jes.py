21b49799b5ed8b87bee91575725882c7ef4b861e
azi-does-things <roryandazi@gmail.com>
2026-01-14 18:17:03 +0800

    Update jes.py and fuck me on my boypussy

--- jes.py
+++ jes.py
@@ -14,13 +14,13 @@ _beats_per_cycle=3, _node_coor_count=4, # x_position, y_position, x_velocity, y_
 _y_clips=[-10000000,0], _ground_friction_coef=25,
 _gravity_acceleration_coef=0.002, _calming_friction_coef=0.7,
 _typical_friction_coef=0.8, _muscle_coef=0.08,
-_traits_per_box=3, # desired width, desired height, rigidity
+_traits_per_box=2, # desired width, desired height, rigidity
 _traits_extra=1, # heartbeat (time)
 _mutation_rate=0.07, _big_mutation_rate=0.025,
 _UNITS_PER_METER=0.05)
 
-# Cosmetic UI variables
-ui = UI(_W_W=1920, _W_H=1078, _MOVIE_SINGLE_DIM=(650,650),
+# Cosmetic UI variables (change this if you want JES to Work!)
+ui = UI(_W_W=1280, _W_H=720, _MOVIE_SINGLE_DIM=(650,650),
 _GRAPH_COOR=(850,50,900,500), _SAC_COOR=(850,560,900,300), _GENEALOGY_COOR=(20,105,530,802,42),
 _COLUMN_MARGIN=330, _MOSAIC_DIM=[10,24,24,30], #_MOSAIC_DIM=[10,10,17,22],
 _MENU_TEXT_UP=180, _CM_MARGIN1=20, _CM_MARGIN2=1)
@@ -37,4 +37,5 @@ while ui.running:
     ui.detectSliders()
     ui.doMovies()
     ui.drawMenu()
+
     ui.show()
